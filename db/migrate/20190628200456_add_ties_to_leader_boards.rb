572614b6570fc3229d5657532ca6c11bb86b8a91
Malachi Irwin <malachi@theirwins.ws>
2020-07-26 09:26:36 -0500

    fix migrations after switching to pg 12

--- db/migrate/20190628200456_add_ties_to_leader_boards.rb
+++
@@ -1,6 +0,0 @@
-class AddTiesToLeaderBoards < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bot_leader_boards, :ties, :integer
-    add_column :user_leader_boards, :ties, :integer
-  end
-end
